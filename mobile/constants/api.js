39a8a1626a717734ff144520b0c87c31bcf6cd0e
Abdul Basit <163563761+theabdulbasitt@users.noreply.github.com>
2026-03-13 01:03:51 +0500

    Configure package.json start script for Railway deployment

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -2,4 +2,4 @@
 // - iOS Simulator:       'http://localhost:3000/api'
 // - Android Emulator:    'http://10.0.2.2:3000/api'
 // - Physical device:     'http://YOUR_LOCAL_IP:3000/api'
-export const API_URL = "http://192.168.0.101:3000/api";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.0.101:3000/api";
